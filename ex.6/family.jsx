56d475c65267b3bc01ad8c72ec2e45f1bd7b2bc2
JAYME <jaymesanches@yahoo.com.br>
2018-12-17 20:26:21 -0200

    adicionado exercícios

--- ex.6/family.jsx
+++ ex.6/family.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { childrenWithProps } from '../utils/reactUtils'
 
 export default props => (
-  <div>
-    <h1>Família</h1>
-    {childrenWithProps(props)}
-  </div>
+    <div>
+        <h1>Family</h1>
+        { childrenWithProps(props.children, props) }
+    </div>
 )
